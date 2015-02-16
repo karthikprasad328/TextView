78cd478e77740c14385737224b91f27b0be3fa13
karthikprasad328 <karthikprasad328@gmail.com>
2015-02-15 21:28:49 -0500

    Initial commit

---
+++ app/src/main/java/app/com/example/karthik/homework1/MainActivity.java
@@ -0,0 +1,84 @@
+package app.com.example.karthik.homework1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //TextView to print the name on the screen
+            TextView textView1= (TextView) rootView.findViewById(R.id.name1);
+            textView1.setTextSize(20);
+            // TextView to print "Hello World"
+            TextView textView2= (TextView) rootView.findViewById(R.id.hello1);
+            textView2.setTextSize(20);
+
+
+            //Set position for the button
+           /* Button my_button=(Button) rootView.findViewById(R.id.button);
+            my_button.setX(400);
+            my_button.setY(400);*/
+
+
+
+
+
+
+            return rootView;
+        }
+    }
+}
